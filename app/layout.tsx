897504d940aca1b2ba642f66cd614c652737114f
dudat2003 <duinhat.03@gmail.com>
2025-07-10 04:34:47 +0700

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { fontSans } from "@/config/fonts";
 import clsx from "clsx";
-import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "ICB ERP System - Hệ thống quản lý khách hàng",
@@ -18,18 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clsx("font-sans antialiased", fontSans.className)}>
-        <Providers>
-          <NextTopLoader
-            color="var(--heroui-primary)"
-            initialPosition={0.05}
-            height={2}
-            showSpinner={false}
-            template='<div class="bar" role="bar" style="background: linear-gradient(to right, hsl(var(--heroui-primary), hsl(var(--heroui-success) ); height: 3px;">
-            <div class="peg"></div>
-          </div>'
-          />
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
